eeef9a6a62f70acd681971445bc8461af59143ea
cesar-mqt <cesar.mouquet@free.fr>
2020-02-28 15:15:57 +0100

    page accueil V1 + mes fiches V1

---
+++ app/src/main/java/com/example/stock_n_go/mesfiches.java
@@ -0,0 +1,14 @@
+package com.example.stock_n_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mesfiches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mesfiches);
+    }
+}
